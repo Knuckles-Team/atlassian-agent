400693f7607ee8a8155559e5b7d97c3982f55693
knucklessg1 <knucklessg1@gmail.com>
2026-05-22 08:17:12 -0500

    Bump version: 0.12.0 → 0.13.0

--- atlassian_agent/mcp_server.py
+++ atlassian_agent/mcp_server.py
@@ -41,7 +41,7 @@ from atlassian_agent.auth import (  # type: ignore
     get_user_provisioning_cloud_client,
 )
 
-__version__ = "0.12.0"
+__version__ = "0.13.0"
 
 logger = get_logger(name="atlassian-agent")
 logger.setLevel(logging.INFO)
